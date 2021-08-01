bb13a4a3656bf6ccd4a869ecf2ce49a1d11b2921
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-08-01 13:45:37 -0500

    Add custom FormInput and Button components and style it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SigninAndSignup from "./pages/signin-and-signup/signin-and-signup.component";
 
 import "./App.css";
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SigninAndSignup} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./signin-and-signup.styles.scss";
+
+const SigninAndSignup = () => (
+  <div className="signin">
+    <SignIn />
+  </div>
+);
+
+export default SigninAndSignup;
